b367c08c7d0fa60981cc7b05b40cf7f1582dc360
marbiru <code@uribram.com>
2015-05-20 13:57:31 +0100

    Total Rewrite as GoogleCharts

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-// http://bost.ocks.org/mike/bar/
+google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['In-Country Income Distribution', 'Global Income Distribution'],
+      [ 2.5,      3750],
+      [ 50,      51939],
+      [ 97.5,     186000],
+    ]);
 
-var data = [10, 1, 15, 16, 23, 42];
+    var options = {
+      title: 'International Income Comparison',
+      hAxis: {title: 'In-Country Income Distribution', minValue: 0, maxValue: 15},
+      vAxis: {title: 'Income in USD (at purchasing power parity)', minValue: 0, maxValue: 15},
+      legend: 'none',
+      colors: ['#087037'],
+      pointSize: 35,
+      pointShape: 'star',
+      animation: {
+        duration: 200,
+        easing: 'inAndOut',
+    }
+    };
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+    var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+    chart.draw(data, options);
+}

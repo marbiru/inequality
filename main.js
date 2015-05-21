a08a5bf18d7cd0fd2739b62bfd8a6f5058799797
marbiru <code@uribram.com>
2015-05-21 11:09:36 +0100

    Main Graph Perfected
    (for now). Woohoo!

--- main.js
+++ main.js
@@ -16,92 +16,111 @@ function draw_chart() {
   context.strokeStyle = "#eee";
   context.stroke();
 
+  var square = 550;
+  var small = 50;
+
   //vertical axis
   context.beginPath();
-  context.moveTo(50, 550);
-  context.lineTo(550, 550);
+  context.moveTo(50, square);
+  context.lineTo(square, square);
+  //vertical arrow
   context.moveTo(495, 495);
   context.lineTo(500, 500);
   context.lineTo(495, 505);
   //vertical axis
-  context.moveTo(50, 50);
-  context.lineTo(50, 550);
+  context.moveTo(small, small);
+  context.lineTo(small, square);
+  //horizontal arrow
   context.moveTo(45, 55);
   context.lineTo(50, 50);
   context.lineTo(55, 55);
   //ticks on horizontal axis
-  context.moveTo(35, 495);
-  context.lineTo(35, 505);
-  context.moveTo(250, 495);
-  context.lineTo(250, 505);
-  context.moveTo(450, 495);
-  context.lineTo(450, 505);
+  context.moveTo(small + 25, square - 5);
+  context.lineTo(small + 25, square + 5);
+  context.moveTo(small + 250, square - 5);
+  context.lineTo(small + 250, square + 5);
+  context.moveTo(small + 475, square - 5);
+  context.lineTo(small + 475, square + 5);
   //ticks on vertical axis
-
-  for (var y = 100.5; y < 550; y += 50) {
+  for (var y = 100.5; y < square; y += 50) {
   context.moveTo(50, y);
   context.lineTo(550.5, y);
   }
-
-
+  //finally, fill in the lines
   context.strokeStyle = "#000";
   context.stroke();
 
   // axis labels
   context.font = "18px sans-serif";
-  context.fillText("Position in National Income Distribution", 90, 580);
-  // context.fillText("Position in International Income Distribution", 15, 10);
+  context.textAlign = 'center';
+  context.fillText("Position in National Income Distribution", small + 250, 600);
+  context.textAlign = 'left';
+  context.fillText("Position in Global Income Distribution", 10, 40);
 
   // horizontal axis labels
   context.font = "14px sans-serif";
-  context.fillText("Bottom 5%", 10, 530);
-  context.fillText("National Average", 220, 530);
-  context.fillText("Top 5%", 430, 530);
+  context.textAlign = 'center';
+  context.fillText("Bottom 5%", small + 25, square + 20);
+  context.fillText("National Average", small + 250, square + 20);
+  context.fillText("Top 5%", small + 475, square + 20);
 
   // vertical axis labels
   for (var y = 100; y < 550; y += 50) {
   context.textBaseline = "middle";
-  context.fillText("X%", 15, y);
+  context.fillText( (550 - y)/5 + "%", 30, y);
   }
 
 
   //India circles
+  //poorest
   context.beginPath();
-  context.arc(75,545,10,0,2*Math.PI);
+  context.arc(small + 25,545,10,0,2*Math.PI);
+  context.fillStyle = "steelBlue";
+  context.fill();
+  //median
+  context.beginPath();
+  context.arc(small + 250,445,10,0,2*Math.PI);
+  context.fillStyle = "steelBlue";
+  context.fill();
+  //richest
+  context.beginPath();
+  context.arc(525,220,10,0,2*Math.PI);
   context.fillStyle = "steelBlue";
   context.fill();
 
+  //US circles
+  //poorest
   context.beginPath();
-  context.arc(300,445,10,0,2*Math.PI);
-  context.fillStyle = "steelBlue";
+  context.arc(small + 25,210,10,0,2*Math.PI);
+  context.fillStyle = "fireBrick";
+  context.fill();
+  //median
+  context.beginPath();
+  context.arc(small + 250,110,10,0,2*Math.PI);
+  context.fillStyle = "fireBrick";
+  context.fill();
+  //richest
+  context.beginPath();
+  context.arc(525,55,10,0,2*Math.PI);
+  context.fillStyle = "fireBrick";
   context.fill();
 
+  //legend
+  //India
   context.beginPath();
-  context.arc(525,205,10,0,2*Math.PI);
   context.fillStyle = "steelBlue";
-  context.fill();
+  context.fillRect(350, 2, 15, 15);
+  context.font = "12 sans-serif";
+  context.textAlign = 'left';
+  context.fillText("= India", 370, 9);
+  //US
+  context.beginPath();
+  context.fillStyle = "fireBrick";
+  context.fillRect(430, 2, 15, 15);
+  context.font = "12 sans-serif";
+  context.textAlign = 'left';
+  context.fillText("= U.S.", 450, 9);
 
 };
 
 draw_chart();
-
- /* draw figures
-  var poorPersonIndia = document.getElementById("poorPersonIndia");
-  context.drawImage(poorPersonIndia, 0, 480);
-
-  var medianPersonIndia = document.getElementById("medianPersonIndia");
-  context.drawImage(medianPersonIndia, 200, 360);
-
-  var richPersonIndia = document.getElementById("richPersonIndia");
-  context.drawImage(richPersonIndia, 400, 100);
-
-  var poorPersonUS = document.getElementById("poorPersonUS");
-  context.drawImage(poorPersonUS, 0, 120);
-
-  var medianPersonUS = document.getElementById("medianPersonUS");
-  context.drawImage(medianPersonUS, 200, 50);
-
-  var richPersonUS = document.getElementById("richPersonUS");
-  context.drawImage(richPersonUS, 400, 0); */
-
-

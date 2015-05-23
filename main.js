5a19ae07d8c00b6b0cdac2a230b0afdcd7fd6bb2
marbiru <code@uribram.com>
2015-05-23 12:29:16 +0100

    fade in of graphs when you scroll down

--- main.js
+++ main.js
@@ -34,13 +34,9 @@ function draw_common_chart(canvas_name) {
   context.moveTo(small - 5, small + 5);
   context.lineTo(small, small);
   context.lineTo(small + 5, small + 5);
-  //ticks on horizontal axis
-  //context.moveTo(small + 25, square - 5);
-  //context.lineTo(small + 25, square + 5);
+  //tick on horizontal axis
   context.moveTo(small + 250, square - 5);
   context.lineTo(small + 250, square + 5);
-  //context.moveTo(small + 475, square - 5);
-  //context.lineTo(small + 475, square + 5);
   //ticks on vertical axis
   for (var y = 100.5; y < square; y += 50) {
   context.moveTo(50, y);
@@ -105,105 +101,8 @@ draw_common_chart( "median_chart" );
 function draw_main_chart() {
   var chart_canvas = document.getElementById("chart");
   var context = chart_canvas.getContext("2d");
-  
-  /* 
-  //grid lines
-  for (var x = 50.5; x < 550.5; x += 10) {
-  context.moveTo(x, 0);
-  context.lineTo(x, 600.5);
-  }
-
-  for (var y = 50.5; y < 550.5; y += 10) {
-  context.moveTo(0, y);
-  context.lineTo(550.5, y);
-  }
-
-  context.strokeStyle = "#eee";
-  context.stroke();
-
-  //horizontal axis
-  context.beginPath();
-  context.moveTo(50, square);
-  context.lineTo(square, square);
-  //horizontal arrow
-  context.moveTo(square - 5, square - 5);
-  context.lineTo(square, square);
-  context.lineTo(square - 5, square + 5);
-  //vertical axis
-  context.moveTo(small, small);
-  context.lineTo(small, square);
-  //vertical arrow
-  context.moveTo(small - 5, small + 5);
-  context.lineTo(small, small);
-  context.lineTo(small + 5, small + 5);
-  //ticks on horizontal axis
-  context.moveTo(small + 25, square - 5);
-  context.lineTo(small + 25, square + 5);
-  context.moveTo(small + 250, square - 5);
-  context.lineTo(small + 250, square + 5);
-  context.moveTo(small + 475, square - 5);
-  context.lineTo(small + 475, square + 5);
-  //ticks on vertical axis
-  for (var y = 100.5; y < square; y += 50) {
-  context.moveTo(50, y);
-  context.lineTo(550.5, y);
-  }
-  //finally, fill in the lines
-  context.strokeStyle = "#000";
-  context.stroke();
-
-  // axis labels
-  context.font = "18px sans-serif";
-  context.fillStyle= "#000";
-  context.textAlign = 'center';
-  context.fillText("Position in National Income Distribution", small + 250, 595);
-  context.textAlign = 'left';
-  context.fillText("Position in Global Income Distribution", 10, 40);
-
-  // horizontal axis median label
-  context.font = "14px sans-serif";
-  context.fillStyle= "#000";
-  context.textAlign = 'center';
-  context.fillText("National Average", small + 250, square + 20);
-
-  // vertical axis labels
-  for (var y = 100; y < 550; y += 50) {
-  context.textBaseline = "middle";
-  context.fillText( (550 - y)/5 + "%", 30, y);
-  }
-
-  //India median
-  context.beginPath();
-  context.arc(small + 250,445,10,0,2*Math.PI);
-  context.fillStyle = "steelBlue";
-  context.fill();
-
-  //US median
-  context.beginPath();
-  context.arc(small + 250,110,10,0,2*Math.PI);
-  context.fillStyle = "fireBrick";
-  context.fill();
-
-  //legend
-  //India
-  context.beginPath();
-  context.fillStyle = "steelBlue";
-  context.fillRect(350, 2, 15, 15);
-  context.font = "12 sans-serif";
-  context.textAlign = 'left';
-  context.fillText("= India", 370, 9);
-  //US
-  context.beginPath();
-  context.fillStyle = "fireBrick";
-  context.fillRect(430, 2, 15, 15);
-  context.font = "12 sans-serif";
-  context.textAlign = 'left';
-  context.fillText("= U.S.", 450, 9);
-  */
 
   //MAIN CHART ONLY
-
-  //TK need to extract connecting lines between datapoints from above
 
   var square = 550;
   var small = 50;
@@ -292,3 +191,30 @@ function draw_main_chart() {
 
 draw_common_chart( "chart" );
 draw_main_chart();
+
+
+// fade in
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var middle_of_object = $(this).offset().top + $(this).outerHeight()/2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > middle_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
